96b041d6eeb2491feb22098a5047e323f60cb07c
Gareth Gillespie <ggillespie88@gmail.com>
2025-06-07 13:57:57 +0100

    palindromes pass

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const cleanInput = input.replace(/[^a-z0-9]/gi, '').toLowerCase();
 
+    const reverse = cleanInput.split('').reverse().join('');
+
+    if(reverse === cleanInput) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

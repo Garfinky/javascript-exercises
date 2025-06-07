ca7d7f8f9ee5a9486004b94340d355239d7c88f3
Gareth Gillespie <ggillespie88@gmail.com>
2025-06-07 14:22:23 +0100

    fibonacci pass

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+ let num = Number(input);
+
+ if (num < 0 || num === NaN){
+    return 'OOPS';
+ };
+
+ if (num === 0) {return 0;};
+ if (num === 1){return 1;};
+
+ let a = 0;
+ let b = 1;
+
+ for(i = 2; i <= num; i++){
+    let next = a + b;
+    a = b;
+    b = next;
+ };
+
+ return b;
 
 };
 
